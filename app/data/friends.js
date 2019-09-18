b3714b900d478878e3a4cbdc274c41b1e4e34570
Johnathan Mundt <johnathanmg1478@gmail.com>
2019-09-18 08:32:56 -0700

    home and friend done

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+var friends = [
+    {
+      name: "Harold Frederick Shipman",
+      photo: "https://en.wikipedia.org/wiki/Harold_Shipman",
+      scores: ["5", "5", "5", "5", "5"]
+    }, 
+    {
+      name: "Bella Gunness",
+      photo: "https://en.wikipedia.org/wiki/Belle_Gunness",
+      scores: ["2", "3", "5", "4", "1"]
+    },
+    {
+      name: "John Wayne Gacy",
+      photo: "https://en.wikipedia.org/wiki/John_Wayne_Gacy",
+      scores: ["2", "2", "4", "1", "1"]
+    }, 
+    {
+      name: "Jeffrey Dahmer",
+      photo: "https://en.wikipedia.org/wiki/Jeffrey_Dahmer",
+      scores: ["1", "5", "2", "3", "4"]
+    },
+  
+  ];
+  
+  
+  
+  module.exports = friends;
