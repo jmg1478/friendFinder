c4644b2def5d71181c297a09bf2fbba764ce36bc
Johnathan Mundt <johnathanmg1478@gmail.com>
2019-09-20 13:46:52 -0700

    questions and route complete

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,22 +2,22 @@ var friends = [
     {
       name: "Harold Frederick Shipman",
       photo: "https://en.wikipedia.org/wiki/Harold_Shipman",
-      scores: ["5", "5", "5", "5", "5"]
+      scores: ["1", "1", "2", "5", "1","1","1","5","1","1"]
     }, 
     {
       name: "Bella Gunness",
       photo: "https://en.wikipedia.org/wiki/Belle_Gunness",
-      scores: ["2", "3", "5", "4", "1"]
+      scores: ["1", "1", "1", "1", "5","5","1","1","1","5"]
     },
     {
       name: "John Wayne Gacy",
       photo: "https://en.wikipedia.org/wiki/John_Wayne_Gacy",
-      scores: ["2", "2", "4", "1", "1"]
+      scores: ["1", "5", "1", "1", "1","1","5","1","1","1"]
     }, 
     {
       name: "Jeffrey Dahmer",
       photo: "https://en.wikipedia.org/wiki/Jeffrey_Dahmer",
-      scores: ["1", "5", "2", "3", "4"]
+      scores: ["5", "1", "4", "1", "1","1","1","1","5","1"]
     },
   
   ];
--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 // node_modules
 var express = require ('express');
-var path = require('path');
-var bodyParser = require('body-parser');
+// var path = require('path');
+// var bodyParser = require('body-parser');
 
 // run and port var
 var app = express();
 var PORT = process.env.PORT || 7845;
 // .use
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.text());
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(bodyParser.json());
+// app.use(bodyParser.text());
+
 // localhost url extentions
 require('./app/routing/apiRoutes')(app);
-require('./app/routing/htmlRoutes')(app);
+// require('./app/routing/htmlRoutes')(app);
 // node port running function
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
